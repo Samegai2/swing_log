3cc125c28ea1c418489cba2b7a10921e44f2b07a
Ubuntu <ubuntu@ip-172-31-14-15.ap-northeast-1.compute.internal>
2026-08-18 13:09:42 +0000

    0818edit model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users,
+         through: :favorites,
+         source: :user
+
   has_one_attached :image
 
   enum :play_style, {
@@ -17,6 +22,10 @@ class Post < ApplicationRecord
   validates :play_style, presence: true
   validates :score, presence: true
   validates :body, presence: true
+
+  def favorited_by?(user)
+    user.present? && favorites.exists?(user_id: user.id)
+  end
 
   def score_label
     if driving_range?
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,52 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_posts,
+          through: :favorites,
+          source: :post
+
+  has_many :active_relationships,
+          class_name: "Relationship",
+          foreign_key: :follower_id,
+          dependent: :destroy,
+          inverse_of: :follower
+
+  has_many :followings,
+          through: :active_relationships,
+          source: :followed
+
+  has_many :passive_relationships,
+          class_name: "Relationship",
+          foreign_key: :followed_id,
+          dependent: :destroy,
+          inverse_of: :followed
+
+  has_many :followers,
+          through: :passive_relationships,
+          source: :follower
+
   normalizes :email_address, with: ->(email_address) { email_address.strip.downcase }
 
   validates :email_address, presence:true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :introduction, length: { maximum: 200 }, allow_blank: true
   validates :name, presence:true
+
+  def follow(user)
+  return if self == user
+
+  active_relationships.find_or_create_by(followed: user)
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed: user)&.destroy
+  end
+
+  def following?(user)
+    followings.exists?(user.id)
+  end
+  
 
   def self.search_for(content, method)
     return all if content.blank?
